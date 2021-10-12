27067fbc900fc66f6b33d9a6006f2b9aa1f028d2
Brandon Sakai <brandon.sakai@gmail.com>
2021-10-12 16:55:53 -0400

    Elephant parsing code draft. Can't test until I figure out the unit testing framework

--- app/src/main/java/com/example/elephantbook_fieldguide/ApiGetter.kt
+++ app/src/main/java/com/example/elephantbook_fieldguide/ApiGetter.kt
@@ -2,37 +2,63 @@ package com.example.elephantbook_fieldguide
 
 import android.app.DownloadManager
 import android.content.Context
+import android.location.GnssAntennaInfo
 import android.os.Build
 import androidx.annotation.RequiresApi
 import com.android.volley.Response
 import com.android.volley.toolbox.DiskBasedCache
 import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.RequestFuture
 import com.android.volley.toolbox.Volley
+import org.json.JSONArray
 import java.security.AccessController.getContext
 import java.time.OffsetDateTime
 import java.time.format.DateTimeFormatter
 import java.util.*
 
 class ApiGetter {
-    val apiUrl = "https://localhost/individuals.json"
+    private val apiUrl = "https://localhost/individuals.json"
 
     private fun parseDate(dateString: String): OffsetDateTime {
-        val cleanedDateString = dateString.trim().replace(' ', 'T')
         return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            OffsetDateTime.parse(cleanedDateString, DateTimeFormatter.ISO_OFFSET_DATE_TIME)
+            OffsetDateTime.parse(dateString, DateTimeFormatter.ISO_OFFSET_DATE_TIME)
         } else {
             TODO("VERSION.SDK_INT < O")
         }
     }
 
-    fun updateElephantData(): Pair<List<Elephant>, List<Location>> {
+    private fun parseApiResponse(response: JSONArray): MutableList<Elephant> {
         val elephants = mutableListOf<Elephant>()
+        for (i in 0 until response.length()) {
+            elephants.add(Elephant(response.getJSONObject(i)))
+        }
+
+        return elephants
+    }
+
+    fun updateElephantData(ctx: Context): Pair<List<Elephant>, List<Location>> {
+        val elephants: MutableList<Elephant>
         val locations = mutableListOf<Location>()
+
+        // https://stackoverflow.com/questions/16904741/can-i-do-a-synchronous-request-with-volley
+        val future = RequestFuture.newFuture<JSONArray>()
+
+        val queue = Volley.newRequestQueue(ctx)
+        JsonArrayRequest(apiUrl, future, future)
+
+        elephants = try {
+            parseApiResponse(future.get())
+        } catch (e: Exception) {
+            // TODO Error handling
+            println(e.message)
+            println(e.stackTrace)
+            mutableListOf<Elephant>()
+        }
 
         /* MOCK */
         locations.add(
             Location(
-                parseDate("2021-08-03 13:49:30.698000+00:00"),
+                parseDate("2021-08-03T13:49:30.698000+00:00"),
                 -1.215647,
                 35.12674,
                 1,
@@ -40,25 +66,10 @@ class ApiGetter {
         )
         locations.add(
             Location(
-                parseDate("2021-01-01 01:01:01.1+00:00"),
+                parseDate("2021-01-01T01:01:01.1+00:00"),
                 1.5,
                 8.9,
                 1,
-            )
-        )
-
-        elephants.add(
-            Elephant(
-                1,
-                "b70T01E808_-403?X00S00?",
-                "FakeElephant",
-            )
-        )
-        elephants.add(
-            Elephant(
-                2,
-                "b__T__E____-____X__S___",
-                "Invisible Elephant"
             )
         )
         /* MOCK */
--- app/src/main/java/com/example/elephantbook_fieldguide/Elephant.kt
+++ app/src/main/java/com/example/elephantbook_fieldguide/Elephant.kt
@@ -1,5 +1,6 @@
 package com.example.elephantbook_fieldguide
 
+import org.json.JSONObject
 import java.time.OffsetDateTime
 import java.util.*
 
@@ -7,4 +8,6 @@ class Elephant(
     val id: Int,
     val seek: String,
     val name: String,
-)
+) {
+    constructor(obj : JSONObject) : this(obj.getInt("id"), obj.getString("seek"), obj.getString("name"))
+}
