83dd61e54aa765b4e933e69c113a7afed1452de4
bianca <bbacchus007@gmail.com>
2021-10-19 17:17:29 -0400

    image view
    got rid of button and replaced with image display. image will not display unless its in res drawable folder

--- app/src/main/java/com/example/elephantbook_fieldguide/MainActivity.kt
+++ app/src/main/java/com/example/elephantbook_fieldguide/MainActivity.kt
@@ -7,5 +7,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+	val simpleImageView = findViewById<View>(R.id.simpleImageView) as ImageView
+        
+	simpleImageView.setContentDescription(R.drawable."compressed_IMG_5085")
+	//^^changes attribute contentDescription
+
+        simpleImageView.setImageResource(R.drawable."compressed_IMG_5085")
+	//^^changes attribute srcCompat
     }
 }
