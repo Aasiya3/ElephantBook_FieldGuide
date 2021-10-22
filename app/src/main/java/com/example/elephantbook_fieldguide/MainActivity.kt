03954777b87dc0c140de28ae0872b7d4fd790c97
Aasiya <syadda.a.husain@gmail.com>
2021-10-22 14:58:26 -0400

    got image to display

--- app/src/main/java/com/example/elephantbook_fieldguide/MainActivity.kt
+++ app/src/main/java/com/example/elephantbook_fieldguide/MainActivity.kt
@@ -2,17 +2,19 @@ package com.example.elephantbook_fieldguide
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 	val simpleImageView = findViewById<View>(R.id.simpleImageView) as ImageView
-        
-	simpleImageView.setContentDescription(R.drawable."compressed_IMG_5085")
+
+        //simpleImageView.setContentDescription("drawable/compressed_p1090053.jpg");
 	//^^changes attribute contentDescription
 
-        simpleImageView.setImageResource(R.drawable."compressed_IMG_5085")
+        simpleImageView.setImageResource(R.drawable.compressed_p1090053)
 	//^^changes attribute srcCompat
     }
 }
