f4a7e091a08b2c29ef57f3e38d8c3c29e2491367
Brandon Sakai <brandon.sakai@gmail.com>
2021-11-01 11:04:57 -0400

    Added map setup code to MainActivity.kt
    That can't be its permanent home (we'll need a separate activity for the Individual view, so it'll go there eventually)

--- app/src/main/java/com/example/elephantbook_fieldguide/MainActivity.kt
+++ app/src/main/java/com/example/elephantbook_fieldguide/MainActivity.kt
@@ -6,25 +6,70 @@ import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.coroutines.*
 import org.osmdroid.config.Configuration
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.Marker
+import org.osmdroid.views.overlay.Polyline
+import java.time.OffsetDateTime
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.elephantpage)
-        val simpleImageView = findViewById<View>(R.id.elephantPicture) as ImageView
 
         // https://github.com/osmdroid/osmdroid/wiki/Important-notes-on-using-osmdroid-in-your-app
         //5.6 and newer
         Configuration.getInstance().userAgentValue = BuildConfig.APPLICATION_ID
 
         // TESTING CODE
+        val simpleImageView = findViewById<ImageView>(R.id.elephantPicture)
         val dbW = DatabaseWrapper(applicationContext)
-        dbW.updateDatabase {
-            simpleImageView.setImageDrawable(dbW.getElephantPfp(5))
-            println(dbW.getElephantsByNamePrefix("F"))
-            println(dbW.getLatestLocation(5))
-        }
+        //dbW.updateDatabase {
+        simpleImageView.setImageDrawable(dbW.getElephantPfp(5))
+        println(dbW.getElephantsByNamePrefix("F"))
+        println(dbW.getLatestLocation(5))
+        //}
+
+        setupMap(findViewById<MapView>(R.id.mapView), dbW, 5)
+
         // TESTING CODE
 
+    }
+
+    private fun addMarker(mapView: MapView, location: Location) {
+        val elephantPoint = GeoPoint(location.latitude, location.longitude)
+        val elephantMarker = Marker(mapView)
+        elephantMarker.position = elephantPoint
+        elephantMarker.snippet = location.dateTime.toString()
+        mapView.overlays.add(elephantMarker)
+    }
+
+    private fun setupMap(mapView: MapView, dbW: DatabaseWrapper, id: Int) {
+        val elephantLocations = dbW.getLocationById(id).sortedBy { it.dateTime }
+        // "If MapView is not provided, infowindow popup will not function unless you set it yourself."
+        // This is desired behavior. Empty infowindows are not useful
+        val polyline = Polyline()
+        // I realize we're iterating twice and constructing GeoPoints twice, but I'm preferring
+        // cleaner code to performance at this point
+        // We do the line before the markers cause otherwise we draw over markers :(
+        for (location in elephantLocations) {
+            polyline.addPoint(GeoPoint(location.latitude, location.longitude))
+        }
+        mapView.overlays.add(polyline)
+
+        for (location in elephantLocations) {
+            addMarker(mapView, location)
+        }
+
+        // If we have any locations, center on that. Otherwise center on default location
+        val recentPoint =
+            if (elephantLocations.isNotEmpty()) {
+                val recentLocation = elephantLocations.last()
+                GeoPoint(recentLocation.latitude, recentLocation.longitude)
+            } else {
+                GeoPoint(-1.49, 35.143889)
+            }
+        mapView.controller.setZoom(11.0)
+        mapView.controller.animateTo(recentPoint)
     }
 }
