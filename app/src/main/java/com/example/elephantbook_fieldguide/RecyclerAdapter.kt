fa911fea8d431364df4f23704925831f73f2ffc0
Aasiya Husain <Syadda.a.husain@gmail.com>
2021-12-03 15:46:14 -0500

    initial code on recyclerView

--- app/src/main/java/com/example/elephantbook_fieldguide/RecyclerAdapter.kt
+++ app/src/main/java/com/example/elephantbook_fieldguide/RecyclerAdapter.kt
@@ -1,41 +1,54 @@
 package com.example.elephantbook_fieldguide
 
+import android.content.Intent
 import android.util.Log
 import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView
 
-class RecyclerAdapter(private val elephants: ArrayList<Elephant>) :
+import androidx.recyclerview.widget.RecyclerView
+//Changed ArrayList to List, don't know if that will cause a problem
+class RecyclerAdapter(private val elephants: List<Elephant>) :
     RecyclerView.Adapter<RecyclerAdapter.ElephantHolder>()  {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerAdapter.ElephantHolder {
-        TODO("Not yet implemented")
+        val inflatedView = parent.inflate(R.layout.seek_recyclerview_item_row, false)
+        return ElephantHolder(inflatedView)
+
     }
 
     override fun onBindViewHolder(holder: RecyclerAdapter.ElephantHolder, position: Int) {
-        TODO("Not yet implemented")
+        val itemElephant = elephants[position]
+        holder.bindElephant(itemElephant)
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-        //get the number of elephants in the database
-        //example: override fun getItemCount() = photos.size
-        //https://www.raywenderlich.com/1560485-android-recyclerview-tutorial-with-kotlin
-        //do later when I hear back from brandon about how to get the size.
-    }
+    override fun getItemCount() = elephants.size
 
     class ElephantHolder(v: View) : RecyclerView.ViewHolder(v), View.OnClickListener{
         private var view: View = v
-        private var elepahants: Elephant? = null
+        private var elephants: Elephant? = null
         init {
             v.setOnClickListener { this }
         }
         override fun onClick(v: View) {
             Log.d("RecyclerView", "CLICK!")
             //This should call an intent for the individual view
+            val context = itemView.context
+            val individualViewIntent = Intent(context, IndividualActivity::class.java)
+            individualViewIntent.putExtra(ELEPHANTS_KEY, (elephants!!).id)
+            context.startActivity(individualViewIntent)
+
         }
         companion object {
-            private val ELEPHANTS_KEY = "ELEPHANT"
+            private val ELEPHANTS_KEY = "elephantId"
         }
+
+        fun bindElephant(elephants: Elephant) {
+            this.elephants = elephants
+            //Picasso.with(view.context).load(elephants.url).into(view.itemImage)
+            view.elephantName.txt = elephants.name
+            //view.lastSeen.txt = Last seen
+            view.Seek.txt = elephants.seek
+        }
+
     }
 
 }
--- app/src/main/java/com/example/elephantbook_fieldguide/SeekSearch.kt
+++ app/src/main/java/com/example/elephantbook_fieldguide/SeekSearch.kt
@@ -2,10 +2,30 @@ package com.example.elephantbook_fieldguide
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+private lateinit var adapter: RecyclerAdapter
+private lateinit var linearLayoutManager: LinearLayoutManager
+
 
 class SeekSearch : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_seek_search)
+
+        linearLayoutManager = LinearLayoutManager(this)
+        findViewById<RecyclerView>(R.id.recyclerView).layoutManager = linearLayoutManager
+
+        // Get the Intent that started this activity and extract the string
+        val message = intent.getStringExtra("searchBy")!!
+
+        val databaseWrapper = DatabaseWrapper.create(applicationContext)
+        val listElephants = databaseWrapper.getElephantsBySeek(message)
     }
+
+
+
+
+
 }
